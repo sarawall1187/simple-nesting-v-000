3e795cab407cf584722422eb626e3a81c074729b
Sara Walker <sarawall1187@gmail.com>
2018-12-19 17:14:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      
  alans_new_info = programmer_hash[:alan_kay]
-    alans_new_info[:known_for] = "GUI"
+alans_new_info[:known_for] = "GUI"
 end
 
 def adding_to_dennis
